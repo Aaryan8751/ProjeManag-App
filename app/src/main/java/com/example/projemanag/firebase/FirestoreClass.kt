7f5bb95cb1d1c85b0cef3527a192e931c8b96bc8
Aaryan Sharma <60098288+Aaryan8751@users.noreply.github.com>
2022-01-29 17:40:30 +0530

    Added Drawer and boards to the main

--- app/src/main/java/com/example/projemanag/firebase/FirestoreClass.kt
+++ app/src/main/java/com/example/projemanag/firebase/FirestoreClass.kt
@@ -1,8 +1,10 @@
 package com.example.projemanag.firebase
 
+import android.app.Activity
 import android.util.Log
-import com.example.projemanag.activities.SignInActivity
-import com.example.projemanag.activities.SignUpActivity
+import android.widget.Toast
+import com.example.projemanag.activities.*
+import com.example.projemanag.models.Board
 import com.example.projemanag.models.User
 import com.example.projemanag.utils.Constants
 import com.google.firebase.auth.FirebaseAuth
@@ -21,22 +23,99 @@ class FirestoreClass {
             }
             .addOnFailureListener{
                 e->
-            Log.e(activity.javaClass.simpleName,"Error writing document")
+            Log.e(activity.javaClass.simpleName,"Error writing document",e)
             }
     }
 
-    fun signInUser(activity: SignInActivity){
+    fun createBoard(activity: CreateBoardActivity,board:Board){
+        mFireStore.collection(Constants.BOARDS)
+            .document()
+            .set(board, SetOptions.merge())
+            .addOnSuccessListener {
+                Log.e(activity.javaClass.simpleName,"Board created successfully")
+                Toast.makeText(activity, "Board Created Successfully.", Toast.LENGTH_SHORT).show()
+                activity.boardCreatedSuccessfully()
+            }
+            .addOnFailureListener {
+                    e->
+                Log.e(activity.javaClass.simpleName,"Error writing document",e)
+            }
+    }
+
+    fun getBoardsList(activity: MainActivity){
+        mFireStore.collection(Constants.BOARDS)
+            .whereArrayContains(Constants.ASSIGNED_TO,getCurrentUserId())
+            .get()
+            .addOnSuccessListener {
+                document->
+                Log.i(activity.javaClass.simpleName,document.documents.toString())
+                val boardList:ArrayList<Board> = ArrayList()
+
+                for(i in document.documents){
+                    val board = i.toObject(Board::class.java)!!
+                    board.documentId = i.id
+                    boardList.add(board)
+                }
+                activity.populateBoardsListToUI(boardList)
+            }
+            .addOnFailureListener{
+                e->
+                activity.hideProgressDialog()
+                Log.e(activity.javaClass.simpleName,"Error while getting the boards",e)
+            }
+    }
+
+    
+    fun updateUserProfileData(activity: MyProfileActivity,userHashMap:HashMap<String,Any>){
+        
+        mFireStore.collection(Constants.USERS)
+            .document(getCurrentUserId())
+            .update(userHashMap)
+            .addOnSuccessListener { 
+                Log.i(activity.javaClass.simpleName,"Profile Data updated successfully")
+                Toast.makeText(activity, "Profile Updated Successfully", Toast.LENGTH_SHORT).show()
+                activity.profileUpdateSuccess()
+            }
+            .addOnFailureListener{
+                e->
+                activity.hideProgressDialog()
+                Log.e(activity.javaClass.simpleName,"Error while creating a board",e)
+                Toast.makeText(activity, "Profile updated Error", Toast.LENGTH_SHORT).show()
+            }
+    }
+
+    fun loadUserData(activity: Activity,readBoardList:Boolean = false){
         mFireStore.collection(Constants.USERS)
             .document(getCurrentUserId())
             .get()
             .addOnSuccessListener { document ->
                 val loggedInUser = document.toObject(User::class.java)
-                if(loggedInUser!=null)
-                    activity.signInSuccess(loggedInUser)
+                if(loggedInUser!=null){
+                    when(activity){
+                        is SignInActivity ->{
+                            activity.signInSuccess(loggedInUser)
+                        }
+                        is MainActivity ->{
+                            activity.updateNavigationUserDetails(loggedInUser,readBoardList)
+                        }
+                        is MyProfileActivity ->{
+                            activity.setUserDataInUI(loggedInUser)
+                        }
+                    }
+                }
+
             }
             .addOnFailureListener{
                 e->
-            Log.e(activity.javaClass.simpleName,"Error writing document")
+                when(activity){
+                    is SignInActivity->{
+                        activity.hideProgressDialog()
+                    }
+                    is MainActivity->{
+                        activity.hideProgressDialog()
+                    }
+                }
+            Log.e(activity.javaClass.simpleName,"Error writing document",e)
             }
     }
 
